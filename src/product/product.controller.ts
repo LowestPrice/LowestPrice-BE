1b06ce6959988a5c0ad9b9140fc8f38cf7474086
jieunpark23 <jenne7700@gmail.com>
2023-10-11 12:44:14 +0900

    Feat: Product 상품 전체 조회 API 추가
    상품 전체 조회 API
    상품 상세 조회 API 기본 내용 구성

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private productService: ProductService) {}
+
+    // 상품 전체 조회 
+    @Get()
+    getAllProducts() {
+        return this.productService.getAllProducts();
+    }
+
+    // 상품 상세 조회
+    @Get('/:id')
+    getProductDetail(@Param('id') id: number) {
+        return this.productService.getProductDetail(id);
+    }
+
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, Product } from '@prisma/client';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    private prisma = new PrismaClient();
+
+    async getAllProducts() {
+        const products = await this.prisma.product.findMany(); // 예를 들어 Prisma를 사용한다고 가정
+
+        return products.map(product => ({
+            ...product,
+            coupangItemId: product.coupangItemId.toString(),  // BigInt를 문자열로 변환
+            coupangVendorId: product.coupangVendorId.toString(), // BigInt를 문자열로 변환
+        }));
+    }
+
+    async getProductDetail(id: number): Promise<Product> {
+        return this.prisma.product.findUnique({
+            where: {
+                productId: id,
+            }
+        });
+    }
+}
