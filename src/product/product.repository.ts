046477dde815dc322907c46857c5ace779f398ad
jieunpark23 <jenne7700@gmail.com>
2023-10-11 17:10:04 +0900

    Feat: ProductRepository 수정 - 데이터
    res 데이터 select, 외래키 연결
    배열 한줄로 펴는 거 적용 전

--- src/product/product.repository.ts
+++ src/product/product.repository.ts
@@ -6,8 +6,35 @@ import { PrismaClient, Product } from "@prisma/client";
 export class ProductRepository {
     constructor(private readonly prisma: PrismaClient) {}
 
-    async getAllProducts(): Promise<Product[]> {
-        return this.prisma.product.findMany();
+    async getAllProducts() {
+        const product = await this.prisma.product.findMany({
+            select: {
+                productId: true,
+                coupangItemId: true,
+                coupangVendorId: true,
+                productName: true,
+                productImage: true,
+                isOutOfStock: true,
+                originalPrice: true,
+                currentPrice: true,
+                ProductCategory: {
+                    select: {
+                        Category: {
+                            select: {
+                                categoryId: true,
+                                name: true,
+                            },
+                        },
+                    },
+                },
+                PriceHistory: {
+                    select: {
+                        discountRate: true,
+                    },
+                },
+            },
+        });
+        return product;
     }
 
     async getProductDetail(productId: number): Promise <Product> {
