0694f71986e3fa249ee74fa68e91db0e29a140d8
jieunpark23 <jenne7700@gmail.com>
2023-10-19 20:32:32 +0900

    Style: search 필요없는 주석 삭제
    search.repository.ts 필요없는 주석 삭제

--- src/search/search.repository.ts
+++ src/search/search.repository.ts
@@ -18,13 +18,6 @@ export class SearchRepository {
       },
     }));
 
-    // // 전체 검색어도 포함 // 이거를 추가하게 되면 AND 로 했을때 이 조건도 충족해야해서 제외
-    // searchCondition.push({
-    //   productName: {
-    //     contains: search,
-    //   },
-    // });
-
     //* 검색결과 조회시 AND 조건으로 조회
     const products = await this.prisma.product.findMany({
       where: {
@@ -62,39 +55,4 @@ export class SearchRepository {
 
     return products;
   }
-
-  //   async searchProduct(search: string) {
-  //     const products = await this.prisma.product.findMany({
-  //       where: {
-  //         productName: {
-  //           contains: search, // 'search'가 포함된 productName을 찾는다. %search%와 같음
-  //         },
-  //       },
-  //       select: {
-  //         productId: true,
-  //         coupangItemId: true,
-  //         coupangVendorId: true,
-  //         productName: true,
-  //         productImage: true,
-  //         isOutOfStock: true,
-  //         originalPrice: true,
-  //         currentPrice: true,
-  //         discountRate: true,
-  //         cardDiscount: true,
-  //         createdAt: true,
-  //         updatedAt: true,
-  //         ProductCategory: {
-  //           select: {
-  //             Category: {
-  //               select: {
-  //                 categoryId: true,
-  //                 categoryName: true,
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     });
-  //     return products;
-  //   }
 }
