716835ce0115239c3d32e3759ee57d801f8b67dc
SHIN1501020 <shin8hh@gmail.com>
2023-10-20 16:46:31 +0900

    Feat: 상품 알림 설정 인증/인가 추가

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,5 +1,21 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { AuthGuard } from '@nestjs/passport';
+import { UnauthorizedExceptionFilter } from 'src/auth/util/decorator/not-user.decorator';
+
+interface CustomRequest extends Request {
+  user: {
+    userId: number;
+  };
+}
 
 @Controller('notification')
 export class NotificationController {
@@ -7,17 +23,26 @@ export class NotificationController {
 
   //! 로그인 jwt 구현되면 /user/:userId 경로는 삭제
   //* 알림 설정한 상품 조회
-  @Get('/user/:userId')
-  findAll(@Param('userId') userId: number): Promise<object> {
+  @Get('/')
+  @UseGuards(AuthGuard('jwt'))
+  @UseFilters(UnauthorizedExceptionFilter)
+  findAll(
+    @Req() req: CustomRequest,
+    @Param('magazineId') magazineId: number
+  ): Promise<object> {
+    const userId: number = req.user.userId;
     return this.notificationSevice.findAll(userId);
   }
 
   //* 상품 알림 설정
-  @Post('/user/:userId/product/:productId')
+  @Post('/product/:productId')
+  @UseGuards(AuthGuard('jwt'))
+  @UseFilters(UnauthorizedExceptionFilter)
   setNotification(
-    @Param('userId') userId: number,
+    @Req() req: CustomRequest,
     @Param('productId') productId: number
   ): Promise<object> {
+    const userId: number = req.user.userId;
     return this.notificationSevice.setNotification(userId, productId);
   }
 }
